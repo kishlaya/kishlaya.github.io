7d65ae168012673b7784ef14da3cb3b4ee0b548f
Kishlaya Jaiswal <kishlaya.j@gmail.com>
2021-10-31 20:40:58 +0530

    divided into different subpages

--- js/main.js
+++ js/main.js
@@ -27,16 +27,27 @@ function initPage() {
   reset(hash);
 }
 
-// function reset(section) {
-//   navClose();
+/**
+  * @param {String} url - address for the HTML to fetch
+  * @return {String} the resulting HTML string fragment
+  */
 
-//   for (i of document.querySelectorAll('#offcanvas_menu li')) {
-//     i.classList.remove("active");
-//   }
-//   document.querySelector('li[data-section=' + section + ']').classList.add("active");
+async function fetchHtmlAsText(url) {
+  return await (await fetch(url)).text();
+}
 
-//   document.getElementById('content').innerHTML = document.getElementById(section).innerHTML;
-// }
+async function reset(section) {
+  navClose();
+
+  for (i of document.querySelectorAll('#offcanvas_menu li')) {
+    i.classList.remove("active");
+  }
+  document.querySelector('li[data-section=' + section + ']').classList.add("active");
+  
+  var pageURL = "pages/" + section + ".html";
+  const contentDiv = document.getElementById("content");
+  contentDiv.innerHTML = await fetchHtmlAsText(pageURL);
+}
 
 function navClose() {
   document.body.classList.remove('offcanvas-open');
@@ -73,26 +84,4 @@ window.onload = function() {
 
   // Load contents of page based on hash in url
   initPage();
-}
-
-/**
-  * @param {String} url - address for the HTML to fetch
-  * @return {String} the resulting HTML string fragment
-  */
-
-async function fetchHtmlAsText(url) {
-  return await (await fetch(url)).text();
-}
-
-async function reset(section) {
-  navClose();
-
-  for (i of document.querySelectorAll('#offcanvas_menu li')) {
-    i.classList.remove("active");
-  }
-  document.querySelector('li[data-section=' + section + ']').classList.add("active");
-  
-  var pageURL = "pages/" + section + ".html";
-  const contentDiv = document.getElementById("content");
-  contentDiv.innerHTML = await fetchHtmlAsText(pageURL);
 }
